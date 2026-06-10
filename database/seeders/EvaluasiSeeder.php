88c8b81926665af14a0c24aeb88f56ce68bf775b
zeno <ariyono281201@gmail.com>
2026-06-10 08:15:11 +0800

    up dikit

--- database/seeders/EvaluasiSeeder.php
+++ database/seeders/EvaluasiSeeder.php
@@ -13,33 +13,33 @@ class EvaluasiSeeder extends Seeder
      */
     public function run(): void
     {
-        // aspek(Pendidikan, Penelitian, Pengabdian)
+        // bidang(Pendidikan, Penelitian, Pengabdian kepada Masyarakat)
         // jenis_laporan(AMI, Monev_jurusan, Survey)
 
         $link = 'https://drive.google.com/file/d/10ut94zuR_s8pcjxp1X9F7iFjkiyhjkSs/view?usp=drive_link';
 
         $jurusan_seeds = [
-            ['aspek' => 'Pendidikan',  'jenis_laporan' => 'AMI',            'jenis' => 'Semester Ganjil', 'id_users' => '1'],
-            ['aspek' => 'Pendidikan',  'jenis_laporan' => 'Monev_jurusan',  'jenis' => 'Semester Genap',  'id_users' => '1'],
-            ['aspek' => 'Pendidikan',  'jenis_laporan' => 'Survey',         'jenis' => 'Tahun',            'id_users' => '1'],
-            ['aspek' => 'Penelitian',  'jenis_laporan' => 'AMI',            'jenis' => 'Semester Ganjil', 'id_users' => '1'],
-            ['aspek' => 'Penelitian',  'jenis_laporan' => 'Monev_jurusan',  'jenis' => 'Tahun',            'id_users' => '1'],
-            ['aspek' => 'Penelitian',  'jenis_laporan' => 'Survey',         'jenis' => 'Semester Genap',  'id_users' => '1'],
-            ['aspek' => 'Pengabdian',  'jenis_laporan' => 'AMI',            'jenis' => 'Semester Ganjil', 'id_users' => '1'],
-            ['aspek' => 'Pengabdian',  'jenis_laporan' => 'Monev_jurusan',  'jenis' => 'Tahun',            'id_users' => '1'],
-            ['aspek' => 'Pengabdian',  'jenis_laporan' => 'Survey',         'jenis' => 'Semester Genap',  'id_users' => '1'],
+            ['bidang' => 'Pendidikan',  'jenis_laporan' => 'AMI',            'jenis' => 'Semester Ganjil', 'id_users' => '1'],
+            ['bidang' => 'Pendidikan',  'jenis_laporan' => 'Monev_jurusan',  'jenis' => 'Semester Genap',  'id_users' => '1'],
+            ['bidang' => 'Pendidikan',  'jenis_laporan' => 'Survey',         'jenis' => 'Tahun',            'id_users' => '1'],
+            ['bidang' => 'Penelitian',  'jenis_laporan' => 'AMI',            'jenis' => 'Semester Ganjil', 'id_users' => '1'],
+            ['bidang' => 'Penelitian',  'jenis_laporan' => 'Monev_jurusan',  'jenis' => 'Tahun',            'id_users' => '1'],
+            ['bidang' => 'Penelitian',  'jenis_laporan' => 'Survey',         'jenis' => 'Semester Genap',  'id_users' => '1'],
+            ['bidang' => 'Pengabdian kepada Masyarakat',  'jenis_laporan' => 'AMI',            'jenis' => 'Semester Ganjil', 'id_users' => '1'],
+            ['bidang' => 'Pengabdian kepada Masyarakat',  'jenis_laporan' => 'Monev_jurusan',  'jenis' => 'Tahun',            'id_users' => '1'],
+            ['bidang' => 'Pengabdian kepada Masyarakat',  'jenis_laporan' => 'Survey',         'jenis' => 'Semester Genap',  'id_users' => '1'],
         ];
 
         $fkip_seeds = [
-            ['aspek' => 'Pendidikan',  'jenis_laporan' => 'AMI',            'jenis' => 'Tahun',            'id_users' => '3'],
-            ['aspek' => 'Pendidikan',  'jenis_laporan' => 'Monev_jurusan',  'jenis' => 'Semester Ganjil', 'id_users' => '3'],
-            ['aspek' => 'Pendidikan',  'jenis_laporan' => 'Survey',         'jenis' => 'Semester Genap',  'id_users' => '3'],
-            ['aspek' => 'Penelitian',  'jenis_laporan' => 'AMI',            'jenis' => 'Tahun',            'id_users' => '3'],
-            ['aspek' => 'Penelitian',  'jenis_laporan' => 'Monev_jurusan',  'jenis' => 'Semester Ganjil', 'id_users' => '3'],
-            ['aspek' => 'Penelitian',  'jenis_laporan' => 'Survey',         'jenis' => 'Semester Genap',  'id_users' => '3'],
-            ['aspek' => 'Pengabdian',  'jenis_laporan' => 'AMI',            'jenis' => 'Tahun',            'id_users' => '3'],
-            ['aspek' => 'Pengabdian',  'jenis_laporan' => 'Monev_jurusan',  'jenis' => 'Semester Ganjil', 'id_users' => '3'],
-            ['aspek' => 'Pengabdian',  'jenis_laporan' => 'Survey',         'jenis' => 'Semester Genap',  'id_users' => '3'],
+            ['bidang' => 'Pendidikan',  'jenis_laporan' => 'AMI',            'jenis' => 'Tahun',            'id_users' => '3'],
+            ['bidang' => 'Pendidikan',  'jenis_laporan' => 'Monev_jurusan',  'jenis' => 'Semester Ganjil', 'id_users' => '3'],
+            ['bidang' => 'Pendidikan',  'jenis_laporan' => 'Survey',         'jenis' => 'Semester Genap',  'id_users' => '3'],
+            ['bidang' => 'Penelitian',  'jenis_laporan' => 'AMI',            'jenis' => 'Tahun',            'id_users' => '3'],
+            ['bidang' => 'Penelitian',  'jenis_laporan' => 'Monev_jurusan',  'jenis' => 'Semester Ganjil', 'id_users' => '3'],
+            ['bidang' => 'Penelitian',  'jenis_laporan' => 'Survey',         'jenis' => 'Semester Genap',  'id_users' => '3'],
+            ['bidang' => 'Pengabdian kepada Masyarakat',  'jenis_laporan' => 'AMI',            'jenis' => 'Tahun',            'id_users' => '3'],
+            ['bidang' => 'Pengabdian kepada Masyarakat',  'jenis_laporan' => 'Monev_jurusan',  'jenis' => 'Semester Ganjil', 'id_users' => '3'],
+            ['bidang' => 'Pengabdian kepada Masyarakat',  'jenis_laporan' => 'Survey',         'jenis' => 'Semester Genap',  'id_users' => '3'],
         ];
 
         foreach (array_merge($jurusan_seeds, $fkip_seeds) as $seed) {
